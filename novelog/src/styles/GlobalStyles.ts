6c577449a5933fc775d9fb0a0be744fb0ce6fa4e
nonelijah <elia341947@gmail.com>
2024-01-23 17:56:17 +0900

    add: style--color 추가

--- novelog/src/styles/GlobalStyles.ts
+++ novelog/src/styles/GlobalStyles.ts
@@ -6,8 +6,22 @@ const GlobalStyle = createGlobalStyle`
 ${reset}
 
 :root {
+    /* color */
+    --color-black: #000000;
+    --color-white: #FFFFFF;
+    --color-orange: #e69f00;
+    --color-skyblue: #56B4E9;
+    --color-green: #0D98BA;
+    --color-yellow: #F0E442;
+    --color-blue: #0072B2;
+    --color-vermilion: #D55E00;
+    --color-pink: #CC79A7;
+    --color-border-b: #767676;
+    --color-border-h: #C4C4C4;
+    --color-bg: #F2F2F2
+
     /* font */
-    --font-family: 'Pretendard', sans-serif
+    --font-family: 'Pretendard', sans-serif;
 }
 
 a {
@@ -23,6 +37,11 @@ button {
 
 button:enabled {
     cursor: pointer;
+    opacity: 0.8;
+}
+
+button:active {
+    box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
 }
 
 body {
